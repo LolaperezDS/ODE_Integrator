d5268d0bea0a27f80d7c75e6d4f818de80f5ae09
Lolaperez <lolaperez1331@gmail.com>
2022-10-02 22:07:55 +0300

    Minor changes

--- Integrator.cpp
+++ Integrator.cpp
@@ -68,7 +68,7 @@ double RungeKutta45(double x, double y, double h, double(*func)(double, double))
     double k2 = h * func(x + h / 2, y + k1 / 2);
     double k3 = h * func(x + h / 2, y + k2 / 2);
     double k4 = h * func(x + h, y + k3);
-    double Dy = (k1 + 2 * k2 + 2 * k3 + k4) / 6;  // тут возможно требуется умножение на шаг
+    double Dy = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
     return Dy;
 }
 
